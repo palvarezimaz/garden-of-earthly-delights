e077f0b6f8a851a35513ef0f9d37d23c27a796c6
Ikilzig <palvarezimaz@gmail.com>
2022-06-24 17:26:45 +1000

    homepage

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div className="About">
+      <h1>About this page</h1>
+      <h3>
+        This app is the sum of diverse tinyapps that I've been building during
+        the Software Engineering Immersive Course with GA, Cohort 53. The idea
+        is to revisit, remember, practice, implement, expand, delete, forget or
+        revive concepts seen during the course.
+      </h3>
+    </div>
+  );
+}
+
+export default About;
