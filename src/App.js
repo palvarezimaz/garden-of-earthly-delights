f3a620e7175d4c5ee4bbaa3b4752e089ca6a0c2e
palvarzimaz <palvarezimaz@gmail.com>
2023-01-16 23:32:40 +1100

    added duck game, still a lot to go

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import Ducks from './components/Ducks'
 import About from './components/About';
 
 function App() {
@@ -8,11 +9,13 @@ function App() {
     <div className="App">
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/ducks">Ducks</Link>
         <Link to="/about">About</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home name={'user'} />} />
+        <Route path="/ducks" element={<Ducks />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
---
+++ src/components/Ducks.js
@@ -0,0 +1,12 @@
+function Ducks() {
+  return (
+    <div className="Ducks">
+      <h1>Duck hunter game</h1>
+      <iframe width="800" height="600"
+        id="duckwars" src="https://duck-wars.surge.sh/" title="Good 'ol duck game"></iframe>
+      <h2>Can you kill 'em all?</h2>
+    </div >
+  );
+}
+
+export default Ducks;
